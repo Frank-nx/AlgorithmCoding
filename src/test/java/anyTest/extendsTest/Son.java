7df0520982d909fd5c023e4e0fbcba32b7d5271e
nixuan <nixuanwb@163.com>
2019-06-23 20:58:21 +0800

    树问题07

---
+++ src/test/java/anyTest/extendsTest/Son.java
@@ -0,0 +1,17 @@
+package anyTest.extendsTest;
+
+import java.util.HashMap;
+
+/**
+ * @program: AlgorithmCoding
+ * @description:
+ * @author: nixuan
+ * @create: 2019-05-17 10:13
+ **/
+public class Son extends Father {
+
+
+    public void f(HashMap map){
+        System.out.println("this is son!");
+    }
+}
